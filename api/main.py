e3d127e253c23148acad910ab7f4b6cb337019e7
Tawain Lanon <tawainlanon@gmail.com>
2023-08-23 21:51:50 +0000

    Category_create/delete/get_functions

--- api/main.py
+++ api/main.py
@@ -1,7 +1,11 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+<<<<<<< HEAD
+from routers import users, expenses, category
+=======
 from authenticator import authenticator
 from routers import users, expenses
+>>>>>>> main
 import os
 
 app = FastAPI()
@@ -19,3 +23,5 @@ app.include_router(users.router)
 app.include_router(authenticator.router)
 
 app.include_router(expenses.router)
+
+app.include_router(category.router)
