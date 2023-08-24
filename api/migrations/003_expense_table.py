ca8f83284c30db0ffc676be55c51c19633bcb7e8
Phil Sobrepena <philsobrepena@gmail.com>
2023-08-24 00:57:17 +0000

    Income endpoints & Table modification

--- api/migrations/003_expense_table.py
+++ api/migrations/003_expense_table.py
@@ -4,7 +4,7 @@ steps = [
         """
         CREATE TABLE expenses (
             id SERIAL PRIMARY KEY NOT NULL,
-            expense_amount FLOAT NOT NULL,
+            expense_amount NUMERIC(10,2) NOT NULL,
             date DATE NOT NULL,
             description TEXT,
             user_id INTEGER NOT NULL,
