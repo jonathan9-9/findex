14a9a54af52f970d7f2359d9500913945f204d6a
Tawain Lanon <tawainlanon@gmail.com>
2023-08-29 01:05:45 +0000

    Front end authlogin

---
+++ ghi/src/MainPage.js
@@ -0,0 +1,13 @@
+function MainPage() {
+    return (
+    <div className="px-4 py-5 my-5 text-center">
+      <h1 className="display-5 fw-bold">Findex</h1>
+      <div className="col-lg-6 mx-auto">
+        <p className="lead mb-4">
+          The best financial application you will ever freaking see!
+        </p>
+      </div>
+    </div>
+    )
+}
+export default MainPage;
