9f1016e148fb1ae9c724371e3ea4c42fc3b3c34b
phil sobrepena <JPLS791@gmail.com>
2023-08-31 13:57:17 -0700

    nav styling

--- ghi/src/Nav.js
+++ ghi/src/Nav.js
@@ -11,27 +11,23 @@ function Nav() {
 
   return (
     <nav>
-      <div className="border border-neutral-600 py-2 px-2">
-        <ul className="flex justify-end items-center space-y-6 space-x-6 h-full">
+      <div className="py-2 px-2">
+        <ul className="flex justify-end items-center space-y-6 space-x-6 h-full relative">
           <div className="w-[71px] h-[71px] left-2 top-0 absolute bg-customGreen rounded-[44.50px]"></div>
-          <div className="left-[102px] top-[-30px] absolute">
-            <span className="text-slate-50 text-[39px] font-semibold">
+          <div className="left-[58px] absolute">
+            <span className="text-slate-50 text-[32px] font-semibold">
               Fin
             </span>
-            <span className="text-emerald-300 text-[39px] font-semibold">
+            <span className="text-emerald-300 text-[32px] font-semibold">
               Dex
             </span>
-          </div>
-
-          <div className="left-[155px] top-[10px] absolute text-slate-50 text-[25px] font-serif">
-            financial index generator
           </div>
 
           {!token ? (
             <>
               <li>
                 <NavLink
-                  className="text-[22px] text-slate-50 font-weather"
+                  className="text-[18px] text-slate-50 font-sans font-medium pr-4"
                   to="/login"
                 >
                   Login
@@ -39,7 +35,7 @@ function Nav() {
               </li>
               <li>
                 <NavLink
-                  className="text-slate-50 text-[22px] font-weather"
+                  className="text-slate-50 text-[18px] font-sans font-medium pr-4"
                   to="/signup"
                 >
                   Sign Up
@@ -49,7 +45,7 @@ function Nav() {
           ) : (
             <li>
               <button
-                className="text-slate-50 text-[22px] font-weather"
+                className="text-slate-50 text-[18px] font-sans font-medium pr-4"
                 onClick={handleLogout}
               >
                 Logout
@@ -59,7 +55,7 @@ function Nav() {
 
           <li>
             <NavLink
-              className="text-slate-50 text-[22px] font-weather"
+              className="text-slate-50 text-[18px] font-sans font-medium pr-4"
               to="/"
             >
               Home
@@ -67,7 +63,7 @@ function Nav() {
           </li>
           <li>
             <NavLink
-              className="text-slate-50 text-[22px] font-weather"
+              className="text-slate-50 text-[18px] font-sans font-medium pr-4"
               to="/income"
             >
               Income
@@ -75,7 +71,7 @@ function Nav() {
           </li>
           <li>
             <NavLink
-              className="text-slate-50 text-[22px] font-weather"
+              className="text-slate-50 text-[18px] font-sans font-medium pr-4"
               to="/expenses"
             >
               Expenses
@@ -83,13 +79,14 @@ function Nav() {
           </li>
           <li>
             <NavLink
-              className="text-slate-50 text-[22px] font-weather"
+              className="text-slate-50 text-[18px] font-sans font-medium pr-4"
               to="/analyzer"
             >
               Analyzer
             </NavLink>
           </li>
         </ul>
+        <div className="absolute bottom-0 left-0 w-full h-[1px] bg-neutral-600 opacity-70" />
       </div>
     </nav>
   );
